563f0a66b2f6721cca52e1d5591efe9f378a5f48
Henrik Lurz <henrik.lurz@web.de>
2020-11-03 19:35:03 +0100

    Added script for starting simulation - Starting sim - Spawning world and block - Spawning robots that are defined by the yaml file

--- mur205_launch/scripts/system_sim.sh
+++ mur205_launch/scripts/system_sim.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 
-#Start the simulation environment
-terminator -e "roslaunch mur205_launch system_sim.launch"&
+get_index_of_colon() {
+    return `expr index "$1" :`
+}
 
-sleep 3
+filename='../config/spawn_config.yaml'
+
+robot_name="Robot0"
+x_pos="0.111"
+y_pos="0.222"
+yaw_rot="0.333"
+counter=0
+
+#Start the simulation environment
+#termiantor -e "roslaunch mur205_launch system_sim.launch"
+terminator -e "roslaunch mur205_launch system_sim.launch"&
+sleep 5
 
 #Spawn the robot models into the simulation environment
 terminator -e "roslaunch control_ur5_test spawn_separated_block.launch"&
-./spawn_robots.sh
 
-sleep 10
+while read p; do 
+    echo $p
+    get_index_of_colon $p
+    index_of_colon=$? # $? Contains return value of previous command
+    echo ${p:$index_of_colon}
+    if (( counter == 0 )); then
+        robot_name=${p:$index_of_colon}
+        counter=$((counter+1))
+    elif (( counter == 1 )); then
+        x_pos=${p:$index_of_colon}
+        counter=$((counter+1))
+    elif (( counter == 2 )); then
+        y_pos=${p:$index_of_colon}
+        counter=$((counter+1))
+    elif (( counter == 3 )); then
+        yaw_rot=${p:$index_of_colon}
+        counter=0
+    fi
 
-#Start the ur5 controller slaves
-./launch_ur5_controller.sh 2&
+    if (( counter == 0 )); then
+        robot_namespace=$robot_name"_ns"
+        robot_tf_prefix=$robot_name"_tf"
+        echo "Spawning robot with the following parameters:"
+        echo "Name: $robot_name | Namespace: $robot_namespace | tf prefix: $robot_tf_prefix"
+        echo "X: $x_pos | Y: $y_pos | yaw: $yaw_rot"
 
-# sleep 10
-# terminator -e "roslaunch ur5_controller ur5_controller_manager.launch"&
-
-echo "Finished setting up sim environment."
+        terminator -e "roslaunch mur205_launch spawn_mur205.launch robot_name:=$robot_name robot_namespace:=$robot_namespace robot_tf:=$robot_tf_prefix x_pos:=$x_pos y_pos:=$y_pos yaw_rot:=$yaw_rot"&
+        sleep 3
+    fi
+done < $filename
 $SHELL
