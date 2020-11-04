30c308962ec26f540a19a471f49e2a8066541045
Henrik Lurz <henrik.lurz@web.de>
2020-11-04 10:52:57 +0100

    Removed depracated scripts

--- mur205_launch/scripts/spawn_robots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-terminator -e "roslaunch mur205_launch spawn_robot0.launch"&
-sleep 1
-terminator -e "roslaunch mur205_launch spawn_robot1.launch"&
-
-echo "Finished spawning robots."
